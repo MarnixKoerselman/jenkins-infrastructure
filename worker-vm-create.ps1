1265a707d4d2c994294775dd4cf990ca81bfdd54
unknown <MarnixKoerselman@users.noreply.gitlab.com>
2019-03-20 12:20:50 +0100

    update

---
+++ worker-vm-create.ps1
@@ -0,0 +1,16 @@
+# https://docs.microsoft.com/en-us/virtualization/hyper-v-on-windows/user-guide/nested-virtualization
+
+# find the VM
+Get-VM
+$MyHypervHostVMName = "Jenkins Worker on Windows Server 2019"
+
+# stop the VM
+Stop-VM $MyHypervHostVMName
+
+# execute this
+Set-VMProcessor -VMName $MyHypervHostVMName -ExposeVirtualizationExtensions $true
+Set-VMNetworkAdapter -VMName $MyHypervHostVMName -MacAddressSpoofing On
+# stop using dynamic memory
+Set-VMMemory $MyHypervHostVMName -DynamicMemoryEnabled $false -StartupBytes 4GB
+
+# the Jenkins worker VM should use the default switch, which allows access to the outside world while being protected by the  default Windows NAT
---
+++ worker-vm-setup.ps1
@@ -0,0 +1,21 @@
+
+# allow simple (or no) password
+secedit.exe /export /cfg secconfig.cfg
+notepad secconfig.cfg
+secedit.exe /configure /db %windir%\securitynew.sdb /cfg secconfig.cfg /areas SECURITYPOLICY
+
+# change the password
+net user administrator *
+
+# remove Windows Defender
+Uninstall-WindowsFeature -Name Windows-Defender -Verbose
+
+# install docker on Windows Server
+Install-Module -Name DockerMsftProvider -Repository PSGallery -Force -Verbose
+Install-Package -Name Docker -ProviderName DockerMsftProvider -Force -Verbose
+Invoke-WebRequest "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\docker\docker-compose.exe
+Restart-Computer
+
+# on the host for the VM, prepare the VM for nested virtualization:
+# https://docs.microsoft.com/en-us/virtualization/hyper-v-on-windows/user-guide/nested-virtualization
+# use: worker-vm-create.ps1
