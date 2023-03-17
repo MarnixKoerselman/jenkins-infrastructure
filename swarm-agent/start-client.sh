0b849eb778e000e514b16d14d6116b3cbc2d369f
Marnix <MarnixKoerselman@users.noreply.github.com>
2023-03-17 17:50:23 +0100

    Use an environment variable to configure the Jenkins Controller URL in Jenkins Swarm Agents.

--- swarm-agent/start-client.sh
+++ swarm-agent/start-client.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# TODO: pass jenkins controller url as parameter to the docker run command
-
 # exit when ctrl-c is used (during debugging/monitoring)
 trap " echo INT received ; exit" INT
 trap " echo SIGTERM received ; exit " SIGTERM
@@ -12,8 +10,8 @@ JENKINS_SWARM_PASSWORD=`cat /run/secrets/JENKINS_SWARM_PASSWORD`
 
 while true
 do
-wget http://controller:8080/swarm/swarm-client.jar -O /swarm-client.jar
-java -jar /swarm-client.jar -executors 1 -url http://controller:8080/ -username $JENKINS_SWARM_USER -password $JENKINS_SWARM_PASSWORD -deleteExistingClients -disableClientsUniqueId -executors 1 -labels "linux ubuntu20.04"
+wget $JENKINS_URL/swarm/swarm-client.jar -O /swarm-client.jar
+java -jar /swarm-client.jar -executors 1 -url $JENKINS_URL -username $JENKINS_SWARM_USER -password $JENKINS_SWARM_PASSWORD -deleteExistingClients -disableClientsUniqueId -executors 1 -labels "linux ubuntu-20.04"
 echo "sleep and retry, swarm-client exit code: $?"
 sleep 10
 done
