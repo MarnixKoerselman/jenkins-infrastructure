6d56590b30f2aaff41765e0df9ed4cdf6f8a4909
Marnix <MarnixKoerselman@users.noreply.github.com>
2023-03-19 14:24:48 +0100

    Various improvements

--- controller/dsl/jobsFromGithub.groovy
+++ controller/dsl/jobsFromGithub.groovy
@@ -1,5 +1,16 @@
 String githubRepoOwner = 'MarnixKoerselman' // take care that the variable name doesn't clash with a DSL reserved name
 organizationFolder(githubRepoOwner) {
+    configure { node ->
+        // Set child scan triggers to 15 minutes
+        node / 'properties' << 'jenkins.branch.OrganizationChildTriggersProperty' {
+            templates {
+                'com.cloudbees.hudson.plugins.folder.computed.PeriodicFolderTrigger' {
+                    spec('H/4 * * * *')
+                    interval('900000')
+                }
+            }
+        }
+    }
     description("This organisation folder contains all repositories in the ${githubRepoOwner} organisation on github :wink:\n\nDepending on the access provided by the credentials \'github-credentials\', you\'ll see only the public repositories - or all.")
     organizations {
         github {
@@ -8,10 +19,29 @@ organizationFolder(githubRepoOwner) {
             credentialsId('github-credentials')
             traits {
                 gitHubBranchDiscovery {
-                    strategyId(1)
+                    strategyId(3)
                 }
                 gitHubPullRequestDiscovery {
-                    strategyId(1)
+                    strategyId(3)
+                }
+                gitHubExcludeArchivedRepositories()
+                gitHubTagDiscovery()
+                pruneStaleBranch()
+                pruneStaleTag()
+                submoduleOption {
+                    extension {
+                        // Retrieve all submodules recursively (uses '--recursive' option which requires git>=1.6.5)
+                        recursiveSubmodules(true)
+                    }
+                }
+                cleanBeforeCheckout {
+                    extension { }
+                }
+                cleanAfterCheckout {
+                    extension {
+                        // Deletes untracked submodules and any other subdirectories which contain .git directories.
+                        deleteUntrackedNestedRepositories(true)
+                    }
                 }
             }
         }
