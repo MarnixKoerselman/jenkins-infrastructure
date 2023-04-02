f8cf192469ca52d6a212e57e8632a284d0aaffc8
Marnix <43467188+MarnixKoerselman@users.noreply.github.com>
2023-04-02 14:56:26 +0200

    Make the startup script for Windows Agent a bit configurable.

--- windows-agent/start.bat
+++ windows-agent/start.bat
@@ -1,2 +1,3 @@
-curl -O http://jenkins.test.local/swarm/swarm-client.jar
+if "%JENKINS_CONTROLLER_IP%" neq "" echo %JENKINS_CONTROLLER_IP% jenkins.test.local >> C:\Windows\System32\drivers\etc\hosts
+curl -O http://jenkins.test.local/swarm/swarm-client.jar || exit /b
 java -jar swarm-client.jar -executors 1 -url http://jenkins.test.local -username swarm-user -password swarm-password -executors 1 -labels "windows x64 docker"
